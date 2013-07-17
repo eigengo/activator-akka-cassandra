74c782267358fed6a3a7d4490122f1a253fbf813
Jan Machacek <jan.machacek@gmail.com>
2013-07-17 21:49:29 +0100

    Cassandra tests now work as expected.

--- src/test/scala/core/TweetWriterActorSpec.scala
+++ src/test/scala/core/TweetWriterActorSpec.scala
@@ -9,17 +9,33 @@ import core.TweetReadActor.FindAll
 
 class TweetWriterActorSpec extends TestKit(ActorSystem())
   with SpecificationLike with TestCassandraCluster with CleanCassandra with ImplicitSender {
+  sequential
 
   val writer = TestActorRef(new TweetWriterActor(cluster))
   val reader = TestActorRef(new TweetReadActor(cluster))
 
   "Slow & steady" >> {
+    def write(count: Int): List[Tweet] = {
+      val tweets = (1 to count).map(id => Tweet(id.toString, "@honzam399", "Yay!", new Date))
+      tweets.foreach(writer !)
+      tweets.toList
+    }
+
     "Single tweet" in {
-      writer ! Tweet("123", "@honzam399", "Yay!", new Date)
+      val tweet = write(1).head
+
       reader ! FindAll
       val res = expectMsgType[Either[ErrorMessage, List[Tweet]]]
-      println(res)
-      success
+      res mustEqual Right(List(tweet))
+    }
+
+    "100 tweets" in {
+      val writtenTweets = write(100)
+
+      reader ! FindAll
+      val readTweets = expectMsgType[Either[ErrorMessage, List[Tweet]]].right.get
+      readTweets must containTheSameElementsAs(writtenTweets)
     }
   }
+
 }
